f3873ff96cb773f33e50be5af5686aa36046315a
Muttabora <glamtanzania@gmail.com>
2025-08-29 01:53:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const path = require('path');
 require('dotenv').config(); // loads .env
 
 // --- Config from .env
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 0000;
 const MONGO_URI = process.env.MONGO_URI;
 const CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
 const CLOUD_KEY = process.env.CLOUDINARY_API_KEY;
@@ -171,4 +171,5 @@ app.post('/api/upload', checkAdmin, upload.single('image'), async (req, res) => 
 // --- Start server
 app.listen(PORT, () => {
   console.log(`?? Server running on http://localhost:${PORT}`);
+
 });
